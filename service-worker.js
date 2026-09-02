f4e8da1543239f19a8b59bfeefe8138e6d9b1a84
Toastinette <adressedemorgan@gmail.com>
2026-09-02 13:03:56 +0400

    Ajoute l’Aperol Spritz

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
    données vivent dans localStorage, jamais sur le réseau.
    Après toute modification des fichiers, incrémenter CACHE.
 ------------------------------------------------------------------- */
-const CACHE = 'molow-v9';
+const CACHE = 'molow-v10';
 
 const SHELL = [
   './',
